0729ae7929d54e3f50ddf00e14a1cee2facb561a
Daniel Graf <33464897+HermitCrabSoftware@users.noreply.github.com>
2021-08-31 19:50:56 -0700

    Database refactor

---
+++ WebCRM/src/WebCRM.Data/CRMDataModelBase.cs
@@ -0,0 +1,33 @@
+namespace  WebCRM.Data
+{
+    using System;
+    using Microsoft.EntityFrameworkCore;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+
+    public abstract class CRMDataModelBase<T>: ICRMDataModel<T>
+        where T: ICRMDataModel<T>
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        public DateTime CreationDate { get; set; }
+
+        public string CreatedBy { get; set; }
+
+        public DateTime? DeletionDate { get; set; }
+
+        public string DeletionBy { get; set; }
+
+        public DateTime? LastUpdatedDate { get; set; }
+
+        public string LastUpdatedBy { get; set; }
+
+        public virtual void RestrictedModelUpdate(T model)
+        {
+            this.DeletionDate =  model.DeletionDate;
+            this.DeletionBy = model.DeletionBy;
+        }
+    }
+}
