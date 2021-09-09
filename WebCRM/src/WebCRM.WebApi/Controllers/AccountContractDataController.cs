03c6c3f7ab29dd006865e079bc002b22adddbed3
Daniel Graf <33464897+HermitCrabSoftware@users.noreply.github.com>
2021-09-08 20:22:59 -0700

    member contracts

---
+++ WebCRM/src/WebCRM.WebApi/Controllers/AccountContractDataController.cs
@@ -0,0 +1,46 @@
+namespace WebCRM.WebApi.Controllers
+{
+    using WebCRM.Data;
+    using WebCRM.Shared;
+    using WebCRM.RoleSecurity;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.Extensions.Logging;
+    using System;
+    using System.Linq;
+
+    /// <summary>
+    /// CRM Api controller for Contract Data
+    /// </summary>
+    /// <author>Daniel Lee Graf</author>
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AccountContractDataController:ControllerBase
+    {
+        private CRMDBContext _ctx;
+        private IAppSecurityService _security
+
+        public AccountContractDataController(IAppSecurityService security, CRMDBContext ctx)
+        {
+            this._security = security;
+            this._ctx = ctx;
+        }
+
+        [HttpGet("{id}")]
+        public override IActionResult Get([FromRoute] int id)
+        {
+            var data = (from c in this._ctx.Contracts
+                        join am in this._ctx.AccountMemberships
+                        on c.AccountMembershipID equals am.Id
+                        join a in this._ctx.CRMAccounts
+                        on am.AccountID equals a.Id
+                        join m in this._ctx.Members
+                        on am.MemberID equals m.Id
+                        where a.Id == id
+                        select new ContractViewModel(c, m.MemberName, a.AccountName))
+                    .ToList();
+                    
+            return Ok(data);
+        }
+    }
+}
